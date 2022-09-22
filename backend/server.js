5a579275c57d32d9046537d5311ba725bc95c467
Deepak Kapoor <nk93555@gmail.com>
2022-09-22 13:50:19 +0530

    Fixed deployment

--- backend/server.js
+++ backend/server.js
@@ -24,10 +24,10 @@ app.get("/fetchNews", async (req, res) => {
 const __dirname1 = path.resolve();
 
 if (process.env.NODE_ENV === "production") {
-    app.use(express.static(path.join(__dirname1, "../build")));
+    app.use(express.static(path.join(__dirname1, "frontend/build")));
 
     app.get("*", (req, res) =>
-        res.sendFile(path.resolve(__dirname1, "build", "index.html"))
+        res.sendFile(path.resolve(__dirname1, "frontend", "build", "index.html"))
     );
 }
 else {
